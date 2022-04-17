de2981e55c611bdc31b5756a2337575f942a98ca
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2022-04-17 19:41:29 +0530

    Fix: Remove globally declared variables
    - Removed 3 variables which were declared globally and were being accessed inside the class.

--- tools/result_consumer.py
+++ tools/result_consumer.py
@@ -1,16 +1,11 @@
 from concurrent.futures import TimeoutError
 from google.cloud import pubsub_v1
 
-# TODO(developer)
-project_id = "text-analysis-323506"
-subscription_id = "test_consumer"
-# Number of seconds the subscriber should listen for messages
-timeout = 15.0
-
 
 class PubSubSubscriber(object):
 
-    def __init__(self, project_id: str, subscription_id: str):
+    def __init__(self, project_id: str, subscription_id: str, timeout: int = 10):
+        self.timeout = timeout
         self.subscriber = pubsub_v1.SubscriberClient()
         self.subscription_path = self.subscriber.subscription_path(project_id, subscription_id)
         self.streaming_pull_future = self.subscriber.subscribe(self.subscription_path, callback=PubSubSubscriber.callback)
@@ -28,7 +23,7 @@ class PubSubSubscriber(object):
             try:
                 # When `timeout` is not set, result() will block indefinitely,
                 # unless an exception is encountered first.
-                self.streaming_pull_future.result(timeout=timeout)
+                self.streaming_pull_future.result(timeout=self.timeout)
             except TimeoutError:
                 self.streaming_pull_future.cancel()  # Trigger the shutdown.
                 self.streaming_pull_future.result()  # Block until the shutdown is complete.
