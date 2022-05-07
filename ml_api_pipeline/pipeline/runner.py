520a315b5237de313e8777f4e5994859f6b07a3a
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2022-05-07 17:59:14 +0530

    Updated the pipeline runner implementation
    Both dataflow runner and direct runners work now.

--- ml_api_pipeline/pipeline/runner.py
+++ ml_api_pipeline/pipeline/runner.py
@@ -8,11 +8,12 @@ from apache_beam import io
 from apache_beam.ml.gcp import naturallanguageml as nlp
 from apache_beam.options import pipeline_options
 from apache_beam.options.pipeline_options import GoogleCloudOptions
+from apache_beam.runners import DirectRunner, DataflowRunner
 
 from ml_api_pipeline.pipeline.components import PipelineComponents
 
 
-class DataFlowSubmitter(object):
+class PipelineBuilder(object):
 
     def __init__(self, args: Namespace):
         self.project = args.project
@@ -24,15 +25,13 @@ class DataFlowSubmitter(object):
         # Setting up the Apache Beam pipeline options.
         self.options = pipeline_options.PipelineOptions(streaming=True, save_main_session=True)
 
+        # Creating apache beam pipeline object
+        self.pipeline = beam.Pipeline(options=self.options)
+
         if args.direct_runner and args.dataflow_runner:
             raise ValueError('Please specify only one of the options. either direct runner or dataflow runner')
 
-        self.runner = 'DirectRunner'
-        if args.dataflow_runner:
-            self.runner = 'DataFlowRunner'
-
     def set_pipeline_options(self):
-
         # Sets the project to the default project in your current Google Cloud environment.
         _, self.options.view_as(GoogleCloudOptions).project = google.auth.default()
 
@@ -50,27 +49,13 @@ class DataFlowSubmitter(object):
         # finally outputting to the sink.
         self.options.view_as(GoogleCloudOptions).temp_location = f"{dataflow_gcs_location}/temp"
 
-    def build_and_run(self):
-
+    def build(self):
         features = nlp.types.AnnotateTextRequest.Features(extract_document_sentiment=True)
 
-        argv = [
-            f'--project={self.project}',
-            f'--job_name=text-parsing-{datetime.now().strftime("%Y%m%d-%H%M%S")}',
-            '--save_main_session',
-            f'--streaming',
-            f'--staging_location=gs://{self.bucket}/text_parsing/staging/',
-            f'--temp_location=gs://{self.bucket}/text_parsing/temp/',
-            f'--region={self.region}',
-            f'--runner={self.runner}'
-        ]
-
-        pipeline = beam.Pipeline(argv=argv)
-
         (
-            pipeline
+            self.pipeline
             | 'Consume messages' >> io.gcp.pubsub.ReadFromPubSub(
-                                                            topic=f'projects/{self.project}/topics/{self.input_topic}')
+                                    topic=f'projects/{self.project}/topics/{self.input_topic}')
             | 'get review' >> beam.Map(PipelineComponents.get_review)
             | 'Strip lines' >> beam.Map(PipelineComponents.strip_lines)
             | 'Remove emojis' >> beam.Map(PipelineComponents.remove_emojis)
@@ -80,7 +65,11 @@ class DataFlowSubmitter(object):
             | 'To result topic' >> beam.io.WriteToPubSub(topic=f'projects/{self.project}/topics/{self.input_topic}')
         )
 
-        pipeline.run()
+    def run(self, runner: str):
+        if runner == "direct":
+            _ = DirectRunner().run_pipeline(self.pipeline, options=self.options).wait_until_finish()
+        else:
+            _ = DataflowRunner().run_pipeline(self.pipeline, options=self.options)
 
 
 def main():
@@ -94,8 +83,15 @@ def main():
     parser.add_argument('--dataflow-runner', required=False, action='store_true')
     args = parser.parse_args()
 
-    runner = DataFlowSubmitter(args=args)
-    runner.build_and_run()
+    pipeline_builder = PipelineBuilder(args=args)
+    pipeline_builder.build()
+
+    if args.direct_runner:
+        pipeline_builder.run(runner='direct')
+    elif args.dataflow_runner:
+        pipeline_builder.run(runner='dataflow')
+    else:
+        raise ValueError("Invalid runner...")
 
 
 if __name__ == '__main__':
