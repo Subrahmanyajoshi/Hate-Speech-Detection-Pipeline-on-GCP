e3a64c35958092ddb435e42411ffb80b4ced2753
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2022-05-15 11:50:34 +0530

    Update pipeline creation implementation

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -2,11 +2,7 @@ from argparse import Namespace
 from datetime import datetime
 
 import apache_beam as beam
-import google
 from apache_beam import io
-from apache_beam.options import pipeline_options
-from apache_beam.options.pipeline_options import GoogleCloudOptions
-from apache_beam.runners import DirectRunner, DataflowRunner
 from google.cloud import bigquery
 
 from pipeline.components import PipelineComponents, ResultsFilter
@@ -30,44 +26,46 @@ class PipelineBuilder(object):
             bigquery.SchemaField("sentiment", "STRING", mode="REQUIRED")
         ]
 
-        # Setting up the Apache Beam pipeline options.
-        self.options = pipeline_options.PipelineOptions(streaming=True, save_main_session=True)
-        self.set_pipeline_options()
-
-        argv = []
-        if args.setup_file:
-            argv.append(f'--setup_file={args.setup_file}')
-
-        # Creating apache beam pipeline object
-        self.pipeline = beam.Pipeline(argv=argv, options=self.options)
-
         if args.direct_runner and args.dataflow_runner:
             raise ValueError('Please specify only one of the options. either direct runner or dataflow runner')
 
-    def set_pipeline_options(self):
-        # Sets the project to the default project in your current Google Cloud environment.
-        _, self.options.view_as(GoogleCloudOptions).project = google.auth.default()
+        self.runner = 'DirectRunner'
+        if args.dataflow_runner:
+            self.runner = 'DataFlowRunner'
 
-        # Sets the Google Cloud Region in which Cloud Dataflow runs.
-        self.options.view_as(GoogleCloudOptions).region = self.region
+        # Setting up the Apache Beam pipeline options.
+        self.options = []
+        self.set_options()
 
-        self.options.view_as(GoogleCloudOptions).job_name = f'sa-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+        if args.setup_file:
+            self.options.append(f'--setup_file={args.setup_file}')
 
-        dataflow_gcs_location = f'gs://{self.project}/{self.options.view_as(GoogleCloudOptions).job_name}'
+        # Creating apache beam pipeline object
+        self.pipeline = beam.Pipeline(argv=self.options)
 
-        # Dataflow Staging Location. This location is used to stage the Dataflow Pipeline and SDK binary.
-        self.options.view_as(GoogleCloudOptions).staging_location = f"{dataflow_gcs_location}/staging"
+    def set_options(self):
 
-        # Dataflow Temp Location. This location is used to store temporary files or intermediate results before
-        # finally outputting to the sink.
-        self.options.view_as(GoogleCloudOptions).temp_location = f"{dataflow_gcs_location}/temp"
+        job_name = f'sa-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+        dataflow_gcs_location = f'gs://{self.project}/{job_name}'
+
+        self.options += [
+            f'--project={self.project}',
+            f'--job_name={job_name}',
+            '--save_main_session',
+            '--streaming',
+            f'--staging_location={dataflow_gcs_location}/staging',
+            f'--temp_location={dataflow_gcs_location}/temp',
+            '--region=us-central1',
+            f'--runner={self.runner}'
+        ]
 
     def build(self):
 
         # Get tweet sentiments
         results = (
                 self.pipeline
-                | 'From PubSub' >> io.gcp.pubsub.ReadFromPubSub(topic='projects/text-analysis-323506/topics/tweets')
+                | 'From PubSub' >>
+                io.gcp.pubsub.ReadFromPubSub(topic=f'projects/text-analysis-323506/topics/{self.input_topic}')
                 | 'Load Tweets' >> beam.Map(PipelineComponents.load_tweet)
                 | 'Preprocess Tweets' >> beam.Map(PipelineComponents.preprocess_tweet)
                 | 'Detect Sentiments' >> beam.Map(PipelineComponents.detect_sentiments)
@@ -81,7 +79,8 @@ class PipelineBuilder(object):
         hate_speech_pubsub = (
                 separated_results['Hate speech']
                 | 'Bytes Conversion' >> beam.Map(PipelineComponents.convert_to_bytes)
-                | 'PS Hate Speech' >> beam.io.WriteToPubSub(topic='projects/text-analysis-323506/topics/sa-results')
+                | 'PS Hate Speech' >>
+                beam.io.WriteToPubSub(topic=f'projects/text-analysis-323506/topics/{self.output_topic}')
         )
 
         hate_speech_bq = (
@@ -98,6 +97,6 @@ class PipelineBuilder(object):
 
     def run(self, runner: str):
         if runner == "direct":
-            _ = DirectRunner().run_pipeline(self.pipeline, options=self.options).wait_until_finish()
+            self.pipeline.run().wait_until_finish()
         else:
-            _ = DataflowRunner().run_pipeline(self.pipeline, options=self.options)
+            self.pipeline.run()
