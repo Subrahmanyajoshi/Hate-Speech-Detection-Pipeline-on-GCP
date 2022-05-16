f097e35e5b8288c6a93801e0274ac0f1202b7638
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2022-05-16 18:48:40 +0530

    Update setup.py
    Added github URL

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(
     name='Hate-Speech-Detection',
     version='0.0.1',
+    url='https://github.com/Subrahmanyajoshi/Hate-Speech-Detection-Pipeline-on-GCP',
     author='Subrahmanya Joshi',
     author_email='subrahmanyajoshi123@gmail.com',
     install_requires=[],
